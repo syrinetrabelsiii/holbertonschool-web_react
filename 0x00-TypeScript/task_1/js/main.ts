56d9d1e9b2fdc79ba1fd7094287d794842ec160c
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-05-02 12:32:12 +0100

    task1

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,53 @@
+// Teacher interface
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+  }
+  
+  // Creating teacher objects
+  const teacher1: Teacher = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: true,
+    yearsOfExperience: 5,
+    location: "New York",
+    contract: true
+  };
+  
+  const teacher2: Teacher = {
+    firstName: "Jane",
+    lastName: "Doe",
+    fullTimeEmployee: false,
+    location: "Los Angeles",
+    salary: 50000
+  };
+  
+  const teachersList: Teacher[] = [teacher1, teacher2];
+  
+  // Rendering table with teachers data
+  const table = document.createElement("table");
+  const tableHead = table.createTHead();
+  const tableHeadRow = tableHead.insertRow();
+  const headers = ["First Name", "Last Name", "Full Time Employee", "Years of Experience", "Location"];
+  headers.forEach(header => {
+    const th = document.createElement("th");
+    th.innerText = header;
+    tableHeadRow.appendChild(th);
+  });
+  
+  const tableBody = table.createTBody();
+  teachersList.forEach(teacher => {
+    const tr = document.createElement("tr");
+    Object.values(teacher).forEach(value => {
+      const td = document.createElement("td");
+      td.innerText = String(value);
+      tr.appendChild(td);
+    });
+    tableBody.appendChild(tr);
+  });
+  
+  document.body.appendChild(table);
