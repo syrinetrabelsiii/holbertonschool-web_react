10ce35cc826126cd80a4958c3cdf520931e761e1
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-05-10 23:34:38 +0100

    task5

---
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+    port: 3000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'images',
+        },
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'dist/index.html',
+    }),
+  ],
+};
---
+++ 0x02-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const App = () => {
+  return <h1>Hello, React!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
