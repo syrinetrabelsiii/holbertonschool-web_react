5d55750e4c7794beb7a72db4efb39faa588de7b6
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2024-02-22 13:32:29 +0100

    task2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64:5]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'images/[name].[hash].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
